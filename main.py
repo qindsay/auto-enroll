804a7137beee057bee1f9477a0f294e44ddc9f62
Lindsay Qin <lindsaymqin@gmail.com>
2025-07-12 16:36:24 -0700

    Completed email notifications, currently refactoring.

--- main.py
+++ main.py
@@ -1,86 +1,9 @@
-import os
-from dotenv import load_dotenv
-import subprocess
 import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 
+def main():
+    while True:
+        print("hi")
+        time.sleep(0.5)
 
-load_dotenv()
-USERNAME = os.getenv('USERNAME')
-PASSWORD = os.getenv('PASSWORD')
-EMAIL = os.getenv('EMAIL')
-
-chrome_options = Options()
-# chrome_options.add_argument("user-data-dir=/Users/lindsayqin/Library/Application Support/Google/Chrome")
-# chrome_options.add_argument("profile-directory=Profile 1")
-
-
-# chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(options=chrome_options) 
-driver.get("https://be.my.ucla.edu/")
-time.sleep(0.5) # Let the user actually see something!
-
-myucla = driver.current_window_handle
-
-username = driver.find_element("id", "logon")
-username.send_keys(USERNAME)
-
-password = driver.find_element("id", "pass")
-password.send_keys(PASSWORD)
-
-driver.find_element("xpath", "//*[@id='sso']/form/div/table/tbody/tr/td[1]/button").click()
-
-time.sleep(3)
-subprocess.run(["osascript", "-e", '''
-            tell application "System Events"
-                key down 53  # Press Escape
-                delay 0.2
-                key up 53    # Release Escape
-            end tell
-        '''])
-time.sleep(1)
-subprocess.run(["osascript", "-e", '''
-            tell application "System Events"
-                key down 53  # Press Escape
-                delay 0.2
-                key up 53    # Release Escape
-            end tell
-        '''])
-
-try:
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div[1]/div[3]/div/button"))).click()
-except:
-    print("Couldn't find Other Options")
-
-try:
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,  "/html/body/div/div/div[1]/div/div[1]/ul/li[5]/a"))).click()
-except:
-    print("Couldn't find phone 2")
-    
-driver.switch_to.new_window('window')
-# driver.get("https://stackoverflow.com/users/login?ssrc=head&returnurl=https%3a%2f%2fstackoverflow.com%2fquestions")
-# driver.find_element("xpath", "//*[@id='openid-buttons']/button[1]").click()
-# email = driver.find_element("xpath", "//*[@id='identifierId']")
-# email.send_keys(EMAIL)
-# driver.find_element("xpath", "//*[@id='identifierNext']/div/button").click()
-
-
-
-
-driver.get("https://voice.google.com/u/0/messages")
-
-driver.find_element("xpath", "//*[@id='header']/div[2]/a[2]").click()
-
-# email = driver.find_element("xpath", "//*[@id='identifierId']")
-# email.send_keys(EMAIL)
-
-# driver.find_element("xpath", "//*[@id='identifierNext']/div/button").click()
-
-
-time.sleep(30)
-driver.quit()
+if __name__ == "__main__":
+    main()
--- notify.py
+++ notify.py
@@ -1,68 +1,108 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.select import Select
-import time
 
 from bs4 import BeautifulSoup
 import requests as rq
+from smtplib import SMTP_SSL as SMTP
+from email.mime.text import MIMEText
 
-# chrome_options = Options()
+import os
+from dotenv import load_dotenv
 
-# # chrome_options.add_argument("--headless")
-# driver = webdriver.Chrome(options=chrome_options) 
-# driver.get("https://sa.ucla.edu/ro/Public/SOC")
-
-# time.sleep(5)
-# selection = driver.find_element("id", "search_by")
-# select_menu = Select(selection)
-# select_menu.select_by_value('classidnumber')
-
-urlOpen = "https://sa.ucla.edu/ro/Public/SOC/Results/ClassDetail?term_cd=25F&subj_area_cd=EC%20ENGR&crs_catlg_no=0116C%20M%20&class_id=439398100&class_no=%20001%20%20"
-urlWaitlist = "https://sa.ucla.edu/ro/Public/SOC/Results/ClassDetail?term_cd=25F&subj_area_cd=COM%20SCI&crs_catlg_no=0181%20%20%20%20&class_id=187787200&class_no=%20001%20%20"
-urlClosed = "https://sa.ucla.edu/ro/Public/SOC/Results/ClassDetail?term_cd=25F&subj_area_cd=COM%20SCI&crs_catlg_no=0163%20%20%20%20&class_id=187699200&class_no=%20001%20%20"
-urls = [urlOpen, urlWaitlist, urlClosed]
-
-previous = {'439398100': [54, 0], '187787200': [14, 1], '187699200': [120, 2]}
-def extract_count_status(row, waitlist):
-    if "Open" in row:
-        of_index = row.index("of")
-        spots = int(row[6:of_index-1])
-        return spots, 0
-    elif "Waitlist" in row:
-        print("row", row)
-        print("waitlist", waitlist)
-        of_index = waitlist.index("of")
-        spots = int(waitlist[:of_index-1])
-        return spots, 1
-    else:
-        open_index = row.index("(")
-        close_index = row.index(")")
-        spots = int(row[open_index+1:close_index])
-        return spots, 2
-
-def save_count(url):
-    r = rq.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-
-    id_tags = soup.find("div", id="class_id_textbook")
-    class_id = str(id_tags.find_all("p")[1])[13:22]
+class notify:
+    def __init__(self):
+        load_dotenv()
+        self.dest = os.getenv('DEST')
+        self.email = os.getenv('EMAIL')
+        self.password = os.getenv('PASSWORD')
     
-    candidates = soup.find_all("td")
-    row = candidates[0]
-    waitlist = candidates[1]
+    def send_email(self, class_name, link):
+        SMTPserver = 'smtp.gmail.com'
+        sender = self.email
+        destination = self.dest
+
+        # typical values for text_subtype are plain, html, xml
+        text_subtype = 'html'
+        try:
+            content=f"""
+                <html>
+                <body>
+                    <p>{class_name} is open! Enroll  
+                    <a href="{link}">here</a>.
+                    </p>
+                </body>
+                </html>
+                """
+            msg = MIMEText(content, text_subtype)
+            msg['Subject']= f"{class_name} OPEN"
+            msg['From']   = "sender" # some SMTP servers will do this automatically, not all
+            
+            conn = SMTP(SMTPserver, 465)
+            conn.set_debuglevel(False)
+            conn.login(self.email, self.password)
+            try:
+                conn.sendmail(sender, destination, msg.as_string())
+                print("Successfully sent email.")
+            finally:
+                conn.quit()
+        except Exception as e:
+            print("Error:", e)  
+
+class enroll:
+    def __init__(self):
+        self.notif = notify()
     
-    seats, status = extract_count_status(row.string, waitlist.string)
+    def extract_count_status(self, row, waitlist):
+        if "Open" in row:
+            of_index = row.index("of")
+            spots = int(row[6:of_index-1])
+            return spots, 0
+        elif "Waitlist" in row:
+            print("row", row)
+            print("waitlist", waitlist)
+            of_index = waitlist.index("of")
+            spots = int(waitlist[:of_index-1])
+            return spots, 1
+        else:
+            open_index = row.index("(")
+            close_index = row.index(")")
+            spots = int(row[open_index+1:close_index])
+            return spots, 2
     
-    if class_id not in previous:
-        previous[class_id] = [seats, status]
-    else:
-        if seats < previous[class_id][0]:
-            print("notify")
-    
+    def save_count(self, url):
+        r = rq.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        id_tags = soup.find("div", id="class_id_textbook")
+        class_id = str(id_tags.find_all("p")[1])[13:22]
         
+        candidates = soup.find_all("td")
+        row = candidates[0]
+        waitlist = candidates[1]
+        
+        seats, status = self.extract_count_status(row.string, waitlist.string)
+        
+        if class_id not in previous:
+            previous[class_id] = [seats, status]
+        else:
+            if seats < previous[class_id][0]:
+                self.notif.send_email(class_id, url) 
+                
     
-for url in urls:
-    save_count(url)
+# urlOpen = "https://sa.ucla.edu/ro/Public/SOC/Results/ClassDetail?term_cd=25F&subj_area_cd=EC%20ENGR&crs_catlg_no=0116C%20M%20&class_id=439398100&class_no=%20001%20%20"
+# urlWaitlist = "https://sa.ucla.edu/ro/Public/SOC/Results/ClassDetail?term_cd=25F&subj_area_cd=COM%20SCI&crs_catlg_no=0181%20%20%20%20&class_id=187787200&class_no=%20001%20%20"
+# urlClosed = "https://sa.ucla.edu/ro/Public/SOC/Results/ClassDetail?term_cd=25F&subj_area_cd=COM%20SCI&crs_catlg_no=0163%20%20%20%20&class_id=187699200&class_no=%20001%20%20"
+# urls = [urlOpen, urlWaitlist, urlClosed]
+
+# previous = {'439398100': [54, 0], '187787200': [14, 1], '187699200': [120, 2]}
+
+
+
+# for url in urls:
+#     save_count(url)
+    
+
 
 
 
